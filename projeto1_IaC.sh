923ced11f14d328b89da0202f564502199e7ff0c
aabeck33 <111699313+aabeck33@users.noreply.github.com>
2022-10-12 23:23:16 -0300

    Correção do usuário calos para carlos

--- projeto1_IaC.sh
+++ projeto1_IaC.sh
@@ -13,7 +13,7 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 echo "Criando os usuários..."
-useradd calos -c "Carlos Beck" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd carlos -c "Carlos Beck" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -c "Maria Beck" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd joao -c "João Beck" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
@@ -30,11 +30,11 @@ chown root:root /projeto1
 chmod 777 /projeto1
 chown root:root /projeto1/publico
 chmod 777 /projeto1/publico
-chown root:adm /projeto1/adm
+chown root:GRP_ADM /projeto1/adm
 chmod 770 /projeto1/adm
-chown root:ven /projeto1/ven
+chown root:GRP_VEN /projeto1/ven
 chmod 770 /projeto1/ven
-chown root:sec /projeto1/sec
+chown root:GRP_SEC /projeto1/sec
 chmod 770 /projeto1/sec
 
 echo "Finalizado com sucesso!"
